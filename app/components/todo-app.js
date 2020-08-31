340d8b5a9d0a20c8c80f5e80f49bd111968b31f3
extellion <allan@guichard.biz>
2020-08-31 15:13:48 +0200

    feat: add action and variable to be used in template

--- app/components/todo-app.js
+++ app/components/todo-app.js
@@ -1,4 +1,13 @@
-import Component from '@glimmer/component';
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { action } from "@ember/object";
 
 export default class TodoAppComponent extends Component {
+  @tracked
+  text = "";
+
+  @action
+  onChange(e) {
+    this.text = e.target.value;
+  }
 }
